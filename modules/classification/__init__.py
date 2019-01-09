7e53ff1f1ad090aaf0e9078ff1ae06fc8188895b
Hannes Kramml <a1503268@unet.univie.ac.at>
2019-01-09 16:17:25 +0100

    Added classification skeleton

---
+++ modules/classification/__init__.py
@@ -0,0 +1,17 @@
+from flask import Blueprint
+
+bp = Blueprint('classification', __name__)
+
+
+class ClassificationModule(object):
+
+    @classmethod
+    def classify_cti(cls, cti):
+        raise NotImplementedError
+
+    @classmethod
+    def train_algorithm(cls, cti):
+        raise NotImplementedError
+
+
+from modules.classification import classification_handler
---
+++ modules/classification/classification_handler.py
@@ -0,0 +1,5 @@
+from modules.classification import bp
+
+@bp.route('/ann/<id>')
+def neuronal_network(id):
+    pass
